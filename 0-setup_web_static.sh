f660f61470bf356072f618c1bdc211c70d5e55e1
Secatricia <3560@holbertonschool.com>
2022-04-29 13:48:20 +0000

    Task 0 change

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 #Bash script that sets up your web servers for the deployment of web_static
-sudo apt-get -y update
-sudo apt install -y nginx
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-ln -sf /data/web_static/current /data/web_static/releases/test/
-touch /data/web_static/releases/test/index.html
-printf %s "<html><head></head><body>Holberton School</body>
-</html>" > /data/web_static/releases/test/index.html
-sudo chown -Rh ubuntu:ubuntu /data
-sudo chgrp -R ubuntu /data/
-sed -i "38i\\\tlocation /hbnb_static/\
- {\n\t\t alias /data/web_static/\
- current/;\n\t}\n" /etc/nginx/sites-available/default
+apt-get -y update
+apt-get -y install nginx
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
+echo "Hello world" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data/
+sed -i "38i\\\tlocation /hbnb_static/ {\n\t\t alias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 service nginx restart
