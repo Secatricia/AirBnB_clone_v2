306c3642ad4700e0bc8a162468b7ed403db1e53a
Secatricia <3560@holbertonschool.com>
2022-04-29 13:43:08 +0000

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,15 +6,12 @@ mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 ln -sf /data/web_static/current /data/web_static/releases/test/
 touch /data/web_static/releases/test/index.html
-printf %s "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
+printf %s "<html><head></head><body>Holberton School</body>
 </html>" > /data/web_static/releases/test/index.html
 sudo chown -Rh ubuntu:ubuntu /data
 sudo chgrp -R ubuntu /data/
-sed -i "38i\\\tlocation /hbnb_static/ {\n\t\t alias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sed -i "38i\\\tlocation /hbnb_static/\
+ {\n\t\t alias /data/web_static/\
+ current/;\n\t}\n" /etc/nginx/sites-available/default
 service nginx start
 
